850519d578dd8522f0b53c1af775e707884b4f89
g-n-x <gnx@tuta.io>
2021-05-17 17:33:01 -0300

    add array as input to todolist-list

--- src/app/todolist-list/todolist-list.component.ts
+++ src/app/todolist-list/todolist-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ITodoList } from '../todolist.interface';
 
 @Component({
@@ -7,23 +7,16 @@ import { ITodoList } from '../todolist.interface';
   styleUrls: ['./todolist-list.component.css']
 })
 export class TodolistListComponent implements OnInit {
-  apple: ITodoList = {
-    title:"りんご",
-    description:"美味しいの果物"
-  };
-  cow: ITodoList = {
-    title:"牛",
-    description:"おいしいの肉"
-  };
-  chocolate: ITodoList = {
-    title:"Chocolate",
-    description:"nham nham"
-  };
-  public todoList: ITodoList[] = [ this.apple, this.cow, this.chocolate ];
+  @Input()
+  public todoList: ITodoList[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addTodo() {
+
   }
 
 }
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITodoList } from '../todolist.interface';
 
 @Component({
   selector: 'app-todolist',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
-
+  apple: ITodoList = {
+    title:"りんご",
+    description:"美味しいの果物"
+  };
+  cow: ITodoList = {
+    title:"牛",
+    description:"おいしいの肉"
+  };
+  chocolate: ITodoList = {
+    title:"Chocolate",
+    description:"nham nham"
+  };
+  orange: ITodoList = {
+    title: "orange",
+    description: "failed"
+  }
+  public todoList: ITodoList[] = [ this.apple, this.cow, this.chocolate, this.orange ];
   constructor() { }
 
   ngOnInit(): void {
